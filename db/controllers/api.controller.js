efa10121392fe99be84586c147516e7f0f7e937a
erascu <e.rashku@gmail.com>
2024-11-25 16:04:00 +0000

    add getTopics controller

--- db/controllers/api.controller.js
+++ db/controllers/api.controller.js
@@ -1,5 +1,12 @@
 const endpointJSON = require("../../endpoints.json");
+const { selectTopics } = require("../model/api.model");
 
 exports.getApi = (req, res) => {
   res.status(200).send({ endpoints: endpointJSON });
+};
+
+exports.getTopics = (req, res) => {
+  selectTopics().then((data) => {
+    res.status(200).send({ topics: data });
+  });
 };
