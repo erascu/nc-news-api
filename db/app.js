d6d3fa8c83e62b6d1a91189c938801b3f060a3fb
erascu <e.rashku@gmail.com>
2024-11-25 17:00:14 +0000

    add article id route and getArticleById controller

--- db/app.js
+++ db/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
-const { getApi, getTopics } = require("./controllers/api.controller");
+const {
+  getApi,
+  getTopics,
+  getArticleById,
+} = require("./controllers/api.controller");
 
 app.get("/api", getApi);
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles/:article_id", getArticleById);
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Not found" });
