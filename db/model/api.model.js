a1450b11a2d9c4d882df404fbf73d5e1bc6aa7a0
erascu <e.rashku@gmail.com>
2024-11-25 17:01:24 +0000

    add selectArticleById model

--- db/model/api.model.js
+++ db/model/api.model.js
@@ -5,3 +5,14 @@ exports.selectTopics = () => {
     return rows;
   });
 };
+
+exports.selectArticleById = (article_id) => {
+  const text = `SELECT * FROM articles WHERE article_id = $1`;
+  const values = [article_id];
+  return db.query(text, values).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "ID Not Found!" });
+    }
+    return rows[0];
+  });
+};
