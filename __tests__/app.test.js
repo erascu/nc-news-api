d8b04e42aae55033648685e98a778609e0ce1425
erascu <e.rashku@gmail.com>
2024-11-25 12:04:02 +0000

    update require links

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,10 @@
 const endpointsJson = require("../endpoints.json");
 /* Set up your test imports here */
-
+const app = require("../db/app");
+const request = require("supertest");
 /* Set up your beforeEach & afterAll functions here */
 
-describe("GET /api", () => {
+describe.only("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
