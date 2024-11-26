c1f460e459d9bd7c61f54f3ce392c5e6503608ac
erascu <e.rashku@gmail.com>
2024-11-26 13:47:00 +0000

    add api/articles route

--- db/app.js
+++ db/app.js
@@ -4,6 +4,7 @@ const {
   getApi,
   getTopics,
   getArticleById,
+  getArticles,
 } = require("./controllers/api.controller");
 
 app.get("/api", getApi);
@@ -11,6 +12,8 @@ app.get("/api", getApi);
 app.get("/api/topics", getTopics);
 
 app.get("/api/articles/:article_id", getArticleById);
+
+app.get("/api/articles", getArticles);
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Not found" });
