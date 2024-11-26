f346cae6a15cdd1cd676257b5af1a9a276aecb5e
erascu <e.rashku@gmail.com>
2024-11-26 13:48:39 +0000

    add GET /api/articles tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -34,7 +34,7 @@ describe("GET /api/topics", () => {
         expect(body.topics).toHaveLength(3);
       });
   });
-  test("200: Returns an array of topic objects with slug and description properties", () => {
+  test.only("200: Returns an array of topic objects with slug and description properties", () => {
     return request(app)
       .get("/api/topics")
       .expect(200)
@@ -72,6 +72,44 @@ describe("GET /api/articles/:article_id", () => {
       .expect(404)
       .then(({ body }) => {
         expect(body.msg).toBe("ID Not Found!");
+      });
+  });
+});
+
+describe.only("GET /api/articles", () => {
+  test("200: Returns an array of articles objects", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.articles).toHaveLength(13);
+        body.articles.forEach((article) => {
+          expect(article).toMatchObject({
+            title: expect.any(String),
+            topic: expect.any(String),
+            author: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+            comment_count: expect.any(Number),
+          });
+        });
+      });
+  });
+  test("400: Responds with 'Bad request' when giving an invalid endpoint", () => {
+    return request(app)
+      .get("/api/articles/create")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad request");
+      });
+  });
+  test("404: Responds with 'Not found' when giving an non-existent endpoint", () => {
+    return request(app)
+      .get("/api/loremipsum")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not found");
       });
   });
 });
