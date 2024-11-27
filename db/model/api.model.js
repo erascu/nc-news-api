340fc812e06c1c8af66afac0f444cbf2fe90499b
erascu <e.rashku@gmail.com>
2024-11-27 14:10:00 +0000

    addComment model add

--- db/model/api.model.js
+++ db/model/api.model.js
@@ -18,14 +18,9 @@ exports.selectArticleById = (article_id) => {
 };
 
 exports.selectArticles = () => {
-  const chosenColumns =
-    "articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url";
-
-  const commentCount = "COUNT(comments.comment_id)::int AS comment_count";
-
   return db
     .query(
-      `SELECT ${chosenColumns}, ${commentCount} FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC`
+      `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::int AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC`
     )
     .then(({ rows }) => {
       return rows;
@@ -38,4 +33,15 @@ exports.selectComments = (article_id) => {
   return db.query(sqlQuery, values).then(({ rows }) => {
     return rows;
   });
+};
+
+exports.addComment = (username, body, article_id) => {
+  return db
+    .query(
+      `INSERT INTO comments(author, body, article_id) VALUES ($1, $2, $3) RETURNING *`,
+      [username, body, article_id]
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
 };
