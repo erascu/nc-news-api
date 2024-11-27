bc3084e46d60c8a7bbe875fdcc8334e63a456375
erascu <e.rashku@gmail.com>
2024-11-27 15:46:23 +0000

    add patch route

--- db/app.js
+++ db/app.js
@@ -7,7 +7,9 @@ const {
   getArticles,
   getComments,
   postComment,
+  patchArticles,
 } = require("./controllers/api.controller");
+const { psqlErr } = require("./errors");
 
 app.get("/api", getApi);
 
@@ -23,7 +25,12 @@ app.use(express.json()); //used for post request only
 
 app.post("/api/articles/:article_id/comments", postComment);
 
+app.patch("/api/articles/:article_id", patchArticles);
+
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Not found" });
 });
+
+app.use(psqlErr);
+
 module.exports = app;
