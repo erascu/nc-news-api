f0090839700a3ff8b22ef0bb7d2c448bd15ccb2d
erascu <e.rashku@gmail.com>
2024-11-27 15:47:15 +0000

    add updateArticles model

--- db/model/api.model.js
+++ db/model/api.model.js
@@ -45,3 +45,14 @@ exports.addComment = (username, body, article_id) => {
       return rows;
     });
 };
+
+exports.updateArticles = (article_id, newVote) => {
+  return db
+    .query(
+      `UPDATE articles SET votes = votes + $2 WHERE article_id = $1 RETURNING *`,
+      [article_id, newVote]
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
