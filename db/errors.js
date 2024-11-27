c2a56f57ce8750de5727424f3ec26f7731472766
erascu <e.rashku@gmail.com>
2024-11-27 17:30:20 +0000

    add one more custom error handler

--- db/errors.js
+++ db/errors.js
@@ -1,5 +1,5 @@
 exports.psqlErr = (err, req, res, next) => {
-  console.log(err);
+  //   console.log(err);
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Bad request" });
   } else if (err.code === "23502") {
