c392110cd6a1d738efc1a6064c91ec941206b75c
erascu <e.rashku@gmail.com>
2024-11-28 11:57:42 +0000

    fix test imports after app.js move

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,6 @@
 const endpointsJson = require("../endpoints.json");
 /* Set up your test imports here */
-const app = require("../db/app");
+const app = require("../app");
 const request = require("supertest");
 const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
